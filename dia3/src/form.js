e5c0c5be2294d60e7deb3d5c1e182b3b9f653ce0
rfso <contato.rfso@gmail.com>
2021-08-19 22:06:28 -0300

    Exercicio 02 (dia 03): Add select input and the color select feature

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -22,3 +22,48 @@ inputName.addEventListener("input", (e) => {
 
   e.target.value = finalInputValue;
 });
+
+//
+// Solução do SEGUNDO exercício
+//
+
+const colorsContainer = document.createElement("div");
+const formEl = document.querySelector('[data-js="form"]');
+const optionsArr = ["red", "blue", "pink", "black", "purple"];
+const selectEl = document.createElement("select");
+
+const createOptionEl = (option) => {
+  const optionEl = document.createElement("option");
+  optionEl.textContent = option;
+  optionEl.value = option;
+  return optionEl;
+};
+
+const createColorDiv = (color) => {
+  const colorDiv = document.createElement("div");
+  colorDiv.style.background = `${color}`;
+  colorDiv.style.width = "6rem";
+  colorDiv.style.height = "6rem";
+  colorDiv.style.marginBottom = "1rem";
+  return colorDiv;
+};
+
+optionsArr.forEach((option) => {
+  const finalOptionEl = createOptionEl(option);
+  selectEl.appendChild(finalOptionEl);
+});
+
+const onSelectChange = (e) => {
+  colorsContainer.innerHTML = "";
+  const selectedOptions = [...e.target.selectedOptions];
+  const selectedValues = selectedOptions.map((option) => {
+    const finalColorDiv = createColorDiv(option.value);
+    colorsContainer.appendChild(finalColorDiv);
+  });
+};
+
+formEl.appendChild(selectEl);
+document.body.appendChild(colorsContainer);
+
+selectEl.setAttribute("multiple", "");
+selectEl.addEventListener("change", onSelectChange, false);
