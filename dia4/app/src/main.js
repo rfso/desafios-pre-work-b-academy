2901c4790c3228cf5e5dedfba0ba3f98faa99272
rfso <contato.rfso@gmail.com>
2021-08-22 15:26:33 -0300

    Exercicio 02 (dia 04): Add initial backend request

--- dia4/app/src/main.js
+++ dia4/app/src/main.js
@@ -1,4 +1,6 @@
-import './style.css'
+import "./style.css";
+
+const apiEndpoint = "http://localhost:3333/cars";
 
 const carsFormEl = document.querySelector('[data-js="third form"]');
 const carsTableBodyEl = document.querySelector('[data-js="cars-table"]');
@@ -8,8 +10,9 @@ const getFormElement = (e) => (elementName) => e.target.elements[elementName];
 const createImage = (value) => {
   const tdEl = document.createElement("td");
   const imgEl = document.createElement("img");
-  imgEl.src = value;
+  imgEl.src = value.src;
   imgEl.width = 100;
+  imgEl.alt = value.alt
   imgEl.style.borderRadius = "0.2rem";
   imgEl.style.marginRight = "0.2rem";
   tdEl.appendChild(imgEl);
@@ -43,19 +46,31 @@ const onSubmit = (e) => {
   e.preventDefault();
   const getElement = getFormElement(e);
 
-  const carElements = [
-    {
-      type: "image",
-      value: getElement("car-image").value,
-      element: getElement("car-image"),
-    },
-    { type: "text", value: getElement("car-brand").value },
-    { type: "text", value: getElement("car-year").value },
-    { type: "text", value: getElement("car-plate").value },
-    { type: "color", value: getElement("car-color").value },
-  ];
+  const data = {
+    carImage: getElement("car-image").value,
+    carBrand: getElement("car-brand").value,
+    carYear: getElement("car-year").value,
+    carPlate: getElement("car-plate").value,
+    carColor: getElement("car-color").value,
+  };
 
+  createTableRow(data);
+  clearAllFields(e);
+  focusAfterSubmit(e);
+};
+
+carsFormEl.addEventListener("submit", onSubmit, false);
+
+const createTableRow = (data) => {
   const trEl = document.createElement("tr");
+
+  const carElements = [
+    { type: "image", value: { src: data.carImage, alt: data.carBrand } },
+    { type: "text", value: data.carBrand },
+    { type: "text", value: data.carYear },
+    { type: "text", value: data.carPlate },
+    { type: "color", value: data.carColor },
+  ];
 
   carElements.forEach((carElement) => {
     const tdEl = elementsTypes[carElement.type](carElement.value);
@@ -63,8 +78,46 @@ const onSubmit = (e) => {
   });
 
   carsTableBodyEl.appendChild(trEl);
-  e.target.reset();
-  carElements[0].element.focus();
 };
 
-carsFormEl.addEventListener("submit", onSubmit, false);
+const createNoCarRow = () => {
+  const trEl = document.createElement("tr");
+  const tdEl = document.createElement("td");
+  const thsLength = document.querySelectorAll("table th").length;
+
+  tdEl.setAttribute("colspan", thsLength);
+  tdEl.textContent = "No cars have been found";
+  tdEl.style.color = "#f36";
+
+  trEl.appendChild(tdEl);
+  carsTableBodyEl.appendChild(trEl);
+};
+
+const clearAllFields = (e) => {
+  e.target.reset();
+};
+
+const focusAfterSubmit = (e) => {
+  const focusImageInput = getFormElement(e)("car-image");
+  focusImageInput.focus();
+};
+
+const getCarsFromApi = async () => {
+  const carsInfoResult = await fetch(apiEndpoint)
+    .then((res) => res.json())
+    .catch((error) => ({ error: true, message: error.message }));
+
+  if (carsInfoResult.error) {
+    console.log("An error has occurred:", carsInfoResult.message);
+    return;
+  }
+
+  if (carsInfoResult.length === 0) {
+    createNoCarRow();
+    return;
+  }
+
+  carsInfoResult.forEach(createTableRow);
+};
+
+getCarsFromApi();
